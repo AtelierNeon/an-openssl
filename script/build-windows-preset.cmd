a870cee634fc698bbecbc40ee4bc30911b8d7c0d
Kene Lin <kenelin@gmail.com>
2023-09-01 07:49:36 +0800

    Apply FindSiblingZLIB module

--- script/build-windows-preset.cmd
+++ script/build-windows-preset.cmd
@@ -2,7 +2,7 @@
 
 setlocal
 echo [Windows] Applying preset options ...
-set MY_PROJECT_OPENSSL_WITH_DISABLED_APPS=ON
+set MY_PROJECT_OPENSSL_WITH_DISABLED_APPS=OFF
 set MY_PROJECT_OPENSSL_WITH_ZLIB=ON
 set MY_PROJECT_ZLIB_WITH_DISABLED_TEST_APPS=ON
 echo [Windows] Applying default options ... DONE
